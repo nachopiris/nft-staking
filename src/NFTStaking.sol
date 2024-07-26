5c4d648368a7118535aa5999338fe94934232dd7
Ignacio Piris <ignaciopirisb@gmail.com>
2024-07-26 12:59:24 -0300

    remove unused import

--- src/NFTStaking.sol
+++ src/NFTStaking.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "forge-std/console.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
